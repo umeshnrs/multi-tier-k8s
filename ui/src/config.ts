b95e9aa149d3a4c24c4251c1b8d3ad6c99605fa8
umeshnagarro <umesh.gupta@nagarro.com>
2025-07-29 08:09:33 +0530

    Update Code for Local Dev

--- ui/src/config.ts
+++ ui/src/config.ts
@@ -1,5 +1,7 @@
+console.log(import.meta.env.VITE_API_URL)
+
 const config = {
-  apiUrl: import.meta.env.VITE_API_URL || 'https://localhost:5001'
+  apiUrl: import.meta.env.VITE_API_URL
 }
 
 export default config 
